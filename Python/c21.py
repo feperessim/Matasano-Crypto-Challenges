364e0ddf8edb80d29bc2d42c7f17d444cb70129b
Felipe <fe.peressim@gmail.com>
2021-11-07 16:24:13 -0300

    Fix: setting correct value to constant

--- Python/c21.py
+++ Python/c21.py
@@ -14,7 +14,7 @@ class MT19937():
             self.m = 397
             self.r = 31
             self.a = 0x9908B0DF
-            self.u = 1
+            self.u = 11
             self.d = 0xFFFFFFFF
             self.s = 7
             self.b = 0x9D2C5680
@@ -42,8 +42,8 @@ class MT19937():
         self.lower_mask = (1 << self.r) - 1
         self.upper_mask = ((1 << self.w) - 1) - self.lower_mask
         self.seed_mt()
-        
 
+        
     def seed_mt(self):
         self.MT[0] = self.seed
 
@@ -56,7 +56,7 @@ class MT19937():
             self.twist()
         y = self.MT[self.index]
         y = y ^ ((y >> self.u) & self.d)
-        y = y ^ ((y << self.u) & self.b)
+        y = y ^ ((y << self.s) & self.b)
         y = y ^ ((y << self.t) & self.c)
         y = y ^ (y >> self.l)
         self.index += 1
